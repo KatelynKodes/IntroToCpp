dd03863c7cae7418bbdbf0046f024809ea45c878
Katelyn <katelynwest225@gmail.com>
2021-11-29 16:55:16 -0600

    Set up Entity class in the Battle Game

---
+++ IntroToCPP/Entity.cpp
@@ -0,0 +1,33 @@
+#include "Entity.h"
+
+Entity::Entity(char icon, float health, float attackPwr, float defensePwr)
+{
+	m_icon = icon;
+	m_health = health;
+	m_attackPower = attackPwr;
+	m_defensePower = defensePwr;
+}
+
+float Entity::TakeDamage(float dmgAmount)
+{
+	float dmgTaken = dmgAmount - GetDefPower();
+
+	if (dmgTaken <= 0)
+	{
+		dmgTaken = 0;
+	}
+
+	m_health -= dmgTaken;
+
+	if (m_health <= 0)
+	{
+		m_health = 0;
+	}
+
+	return dmgTaken;
+}
+
+float Entity::attack(Entity entity)
+{
+	return entity.TakeDamage(entity.GetAttkPower());
+}
---
+++ IntroToCPP/Entity.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+class Entity
+{
+public:
+    Entity(char icon, float health, float attackPwr, float defensePwr);
+    char getIcon() { return m_icon; }
+    float getHealth() { return m_health; }
+    float GetAttkPower() { return m_attackPower; }
+    float GetDefPower() { return m_defensePower; }
+
+    float TakeDamage(float dmgAmount);
+    float attack(Entity entity);
+
+private:
+    char m_icon;
+    float m_health;
+    float m_attackPower;
+    float m_defensePower;
+};
